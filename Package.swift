56756e00aab701ad01efa30e30f91f63f4ff104e
Callum McColl <callum.mccoll1@gmail.com>
2019-03-11 16:37:14 +1000

    Converted swift_wb to package version 4

--- Package.swift
+++ Package.swift
@@ -1,8 +1,14 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "GUSimpleWhiteboard",
     dependencies: [
-        .Package(url: "ssh://git.mipal.net/git/CGUSimpleWhiteboard", majorVersion: 1)
+        .package(url: "ssh://git.mipal.net/git/CGUSimpleWhiteboard", .branch("master"))
+    ],
+    targets: [
+        .target(name: "GUSimpleWhiteboard", dependencies: []),
+        .testTarget(name: "GUSimpleWhiteboardTests", dependencies: ["GUSimpleWhiteboard"])
     ]
 )
