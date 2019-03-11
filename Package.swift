5932e7b2021d45620a6354c76c792fc4f8b65841
Callum McColl <callum.mccoll1@gmail.com>
2019-03-11 17:01:35 +1000

    Added product to Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "GUSimpleWhiteboard",
+    products: [
+        .library(
+            name: "GUSimpleWhiteboard",
+            targets: ["GUSimpleWhiteboard"]
+        )
+    ],
     dependencies: [
         .package(url: "ssh://git.mipal.net/git/CGUSimpleWhiteboard", .branch("master"))
     ],
